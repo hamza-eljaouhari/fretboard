bb66a053abc8be242e645e5cbca64a42f4d5588f
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2021-02-09 22:39:43 +0100

    Unfinished spaghetti code. Aghhhhhh !

--- src/components/CircleOfFifths.js
+++ src/components/CircleOfFifths.js
@@ -1,61 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './circle-of-fifths.css'
 import guitar from '../config/guitar';
  
 function CircleOfFifths({pageX, pageY}){
     
-    const [width, setWidth] = useState(1000);
-    const [height, setHeight] = useState(1000);
-    const [resize, setResize] = useState(false);
-    const [circleOfFifthsOffsets, setCircleOfFifthsOffsets] = useState({});
+    const circleOfFifths = useRef(null);
 
-    const svgHeight = 1000;
-    const svgWidth = svgHeight;
+    const width = 500;
+    const height = 500;
     const fontSize = 26;
-    const strokeWidth = 240;
-    const innerCircleRadius = svgWidth / 4 - 10; // 240
+    const strokeWidth = 10;
+    const innerCircleRadius = width / 4 - 10; // 240
     const greenOutterOutlinePadding = 60;
-    const outterOulineRadius = 420;
+    const outterOulineRadius = 230;
     const greenCircleRadius = innerCircleRadius + greenOutterOutlinePadding;
-    const outterCircleRadius = 350;
-    const xPositionCenterCircle = svgWidth / 2; // 500
-    const yPositionCenterCircle = svgWidth / 2; // 500
+    const outterCircleRadius = 200;
+    const xPositionCenterCircle = width / 2; // 500
+    const yPositionCenterCircle = width / 2; // 500
     const outterCircleCenter = Math.sqrt(outterCircleRadius / ( 2 * Math.PI)) + yPositionCenterCircle;
     const innerCircleCenter = Math.sqrt(innerCircleRadius / ( 2 * Math.PI)) + xPositionCenterCircle;
       
-    function resizeCircle(e){
-        if(!resize){
-            return;
+    useEffect(() => {
+        function updateWidthAndHeight(e) {
+            console.log("eeeeeeeeeeeee", e)
         }
 
-        var newWidth = e.pageX;
-        var newHeight = e.pageY;
-
-        setWidth(newWidth);
-        setHeight(newHeight)
-    }
-
-    function mouseUp(){
-        setResize(false);
-    }
-
-    useEffect(() => {
-        console.log("widthoooo", width)
-    }, [width])
-
-    function mouseDown(e){
-        setResize(true)
-        console.log(e.target.parentElement)
-        setCircleOfFifthsOffsets({
-            top: e.target.parentElement.offsetTop,
-            left: e.target.parentElement.offsetLeft
-        })
-
-        console.log({
-            top: e.target.parentElement.offsetTop,
-            left: e.target.parentElement.offsetLeft
-        })
-    }
+        if (circleOfFifths && circleOfFifths.current) {
+            circleOfFifths.current.addEventListener("onchange", updateWidthAndHeight, false);
+            
+            return function cleanup() {
+                circleOfFifths.current.removeEventListener("resize", updateWidthAndHeight, false);
+            };
+        }
+    }, [])
 
     function getCircle(center, radius, steps = 12){
         var elements = []
@@ -90,23 +67,21 @@ function CircleOfFifths({pageX, pageY}){
     })
 
     return(
-            <div onMouseOut={resizeCircle} onMouseMove={resizeCircle} className="circleOfFifths" >
-                <svg style={{height: height, minWidth: width}} >
+            <div id="circleOfFifths" style={{height: height, width: width}} >
+                <svg height="100%" width="100%" style={{transform: `scale(1, 1)`}}>
                     <circle  r={outterOulineRadius} fill="white" strokeWidth="2" stroke="black" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
                     
-                    <circle transform="rotate(15, 500, 500)" r={greenCircleRadius} fill="#fff" strokeWidth={strokeWidth} stroke="darkslategrey" strokeDasharray="1107 1885" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
+                    <circle transform="rotate(15, 250, 250)" r={greenCircleRadius} fill="#fff" strokeWidth={strokeWidth} stroke="darkslategrey" strokeDasharray="1107 1885" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
                     
                     <circle  r={greenCircleRadius} fill="#fff" strokeWidth="2" stroke="darkslategrey" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
                     
-                    <circle id="dashes-circle" r={greenCircleRadius} fill="rgba(1,1,1,0.2)" strokeWidth={strokeWidth} stroke="#0F0F0F" strokeDasharray="1 157" transform="rotate(15, 500, 500)" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
+                    <circle id="dashes-circle" r={greenCircleRadius} fill="rgba(1,1,1,0.2)" strokeWidth={strokeWidth} stroke="#0F0F0F" strokeDasharray="1 157" transform="rotate(15, 250, 250)" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
 
                     <circle  r={innerCircleRadius - 50} fill="white" strokeWidth="2" stroke="black" cx={xPositionCenterCircle} cy={yPositionCenterCircle}/>
 
                     {relatives}
                     {keys}
                 </svg>
-
-                <div id="resizer" onMouseDown={mouseDown} onMouseUp={mouseUp}>|||</div>
             </div>
     );
 }
