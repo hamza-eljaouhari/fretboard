fcda270464ed7e6f85a85bc372929ed1be245a2d
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2023-07-29 21:43:56 +0200

    React extension not detecting state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
-import { createStore } from "redux";
 import rootReducer from "./reducers/index";
+import { createStore, applyMiddleware, compose } from 'redux';
 
-export default createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware()
+  ));
