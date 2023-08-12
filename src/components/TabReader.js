c5f067aabb064998eb1a8a351243bca4dda4e23d
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2023-08-12 14:38:46 +0200

    Read tabs concurrently

--- src/components/TabReader.js
+++ src/components/TabReader.js
@@ -1,99 +1,132 @@
-import TextField from '@material-ui/core/TextField';
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import { connect } from "react-redux";
-
-import { 
-    toggleNote
-} from "../redux/actions";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { toggleNote } from '../redux/actions';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles((theme) => ({
-    formElement: {
-        flex: '1 0 21%',
-        margin: '10px'
-    },
+  formElement: {
+    flex: '1 0 21%',
+    margin: '10px'
+  },
 }));
 
-function TabReader({toggleNote}){
-    const [tabs, setTabs] = useState("e|--------------------------------------------------------------------|\nB|-------10h12b15---10-12----------7----------------------------------|\nG|--/11-------------------9----7-9---7--------------------------------|\nD|-------------------------------------8----9h11p9p7b8----------------|\nA|--------------------------------------------------------------------|\nE|--------------------------------------------------------------------|`");
-    const classes = useStyles();
+function TabReader({ toggleNote }) {
+  const [tabs, setTabs] = useState(`e|--------------------------------------------------------------------|
+B|-------10h12b15---10-12----------7----------------------------------|
+G|--/11-------------------9----7-9---7--------------------------------|
+D|-------------------------------------8----9h11p9p7b8----------------|
+A|--------------------------------------------------------------------|
+E|--------------------------------------------------------------------|`);
 
-    const readTabs = () => {
-        var explodedStrings = tabs.split('\n');
+const [practiceRate, setPracticeRate] = useState(1000);
+const [shouldReadTabs, setShouldReadTabs] = useState(true);
+const classes = useStyles();
 
-        const tuning = explodedStrings.map((string) => {
-            return string[0];
-        });
+  const parseTabs = (tabs) => {
+    const explodedStrings = tabs.split('\n');
 
-        console.log("tuning : ", tuning);
-
-        // extract the notes
-        var notesFrets = explodedStrings.map((string, stringIndex) => {
-            return [...string.matchAll('[0-9]{1,2}|-')].map((match) => {
-                var note = parseInt(match[0]);
-
-                if(isNaN(note)){
-                    return null;
-                }else {
-                    return {
-                        string: stringIndex,
-                        fret: note
-                    };
-                }
-            });
-        });
-
-        // flip array to read in correct order of appearance (from left to right)
-        var flippedArray = notesFrets[0].map((_, colIndex) => notesFrets.map((row) => row[colIndex]));
-
-        // extact notes
-        const notes = [].concat.apply([], flippedArray).filter((note) => {
-            return note !== null && note !== undefined;
-        });
-
-        // play notes
-        notes.forEach((note, noteIndex) => {
-            setTimeout(() => {
-                toggleNote(note.string, note.fret);
-                setTimeout(() => {
-                    toggleNote(note.string, note.fret);
-                }, 2000)
-            }, 2000 * noteIndex)
-        })
-    }
-
-    return (
-        <section>
-            <TextField 
-                rows={12}
-                multiline={true}
-                id="filled-basic" 
-                label="Filled" 
-                variant="filled" 
-                value={tabs}
-                fullWidth={true}
-            />
-            <Button
-                className={classes.formElement}
-                variant="contained"
-                color="primary"
-                size="medium"
-                onClick={readTabs}
-            >
-                Read tabss 
-            </Button>
-        </section>
+    const notesFrets = explodedStrings.map((string) =>
+      [...string.matchAll(/[0-9]{1,2}|-/g)].map((match) =>
+        match[0] === '-' ? null : parseInt(match[0])
+      )
     );
+
+    const notes = [];
+    for (let colIndex = 0; colIndex < notesFrets[0].length; colIndex++) {
+      for (let rowIndex = 0; rowIndex < notesFrets.length; rowIndex++) {
+        const note = notesFrets[rowIndex][colIndex];
+        if (note !== null && note !== undefined) {
+          notes.push({ string: rowIndex, fret: note });
+        }
+      }
+    }
+    return notes;
+  };
+
+  const readTabs = () => {
+
+    if(shouldReadTabs === true){
+      const notes = parseTabs(tabs);
+      setShouldReadTabs(false);
+
+      notes.forEach((note, noteIndex) => {
+        setTimeout(() => {
+          toggleNote(note.string, note.fret);
+          setTimeout(() => {
+            toggleNote(note.string, note.fret);
+          }, practiceRate);
+        }, practiceRate * noteIndex);
+      });
+      
+      setTimeout(() => {
+        setShouldReadTabs(true);
+      }, practiceRate * notes.length);
+    }
+  };
+
+  return (
+    <section>
+      <TextField
+        rows={12}
+        multiline={true}
+        id="filled-basic"
+        label="Guitar Tabs"
+        variant="filled"
+        value={tabs}
+        fullWidth={true}
+        onChange={(e) => setTabs(e.target.value)}
+      />
+      <FormControl 
+        className={classes.formElement}
+        variant="outlined" margin="normal">
+        <Button
+          className={classes.formElement}
+          variant="contained"
+          color="primary"
+          size="medium"
+          onClick={readTabs}
+        >
+          Play Tabs
+        </Button>
+      </FormControl>
+      <FormControl 
+        className={classes.formElement}
+        variant="outlined" margin="normal">
+        <InputLabel htmlFor="practce-rate">Rate in ms :</InputLabel>
+        <Select
+        id="practce-rate"
+        native
+        value={practiceRate}
+        onChange={(e) => {
+          if(shouldReadTabs
+            ){
+            setPracticeRate(e.target.value)
+          }
+        }}
+        label="Keys :"
+        >
+        {
+          [
+            1000,
+            1500,
+            2000,
+            2500,
+            3000
+          ].map((value) => {
+            return <option key={value} >{value}</option>
+          })
+        }
+        </Select>
+      </FormControl>
+                    
+    </section>
+  );
 }
 
-const mapStateToProps = state => {
-    return state;
-};
-
-export default connect(
-    mapStateToProps,
-    { 
-        toggleNote
-    }
-)(TabReader);
+export default connect(null, { toggleNote })(TabReader);
+    
